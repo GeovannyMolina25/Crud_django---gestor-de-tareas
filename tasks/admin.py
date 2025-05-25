6efc9dd8d39bbbf258979c9d216d1451c0d446bb
Nelson Molina <geovannysangucho25@gmail.com>
2025-05-25 18:43:07 -0500

    Subir el proyecto

---
+++ tasks/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Tasks
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', )
+# Register your models here.
+admin.site.register(Tasks, TaskAdmin)
